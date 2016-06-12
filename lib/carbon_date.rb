99529caeaba20c52e6967e085fe96fafaeb8cbe7
Bradley Marques <bradmarxmoosepi@gmail.com>
2016-06-12 20:37:54 +0200

    Added more comments and example usage to formatter.rb

--- lib/carbon_date.rb
+++ lib/carbon_date.rb
@@ -3,6 +3,13 @@ require 'carbon_date/formatter'
 require 'carbon_date/standard_formatter'
 require 'carbon_date/version'
 
+##
+# Provides a custom implmentation of +Date+ with precision.
+#
+# Classes:
+# - +Date+
+# - +Formatter+
+# - +StandardFormatter+ < +Formatter+
 module CarbonDate
 
 
